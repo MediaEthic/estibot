f3b4d0f79fa2a8969a1de5a255f2c091a0249211
MediaEthic <emilie.rozis@erp-ethic.com>
2019-08-02 14:22:00 +0200

    :sparkles: Introducing backend authentification

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -70,9 +70,9 @@ $app->singleton(
 //     App\Http\Middleware\ExampleMiddleware::class
 // ]);
 
-// $app->routeMiddleware([
-//     'auth' => App\Http\Middleware\Authenticate::class,
-// ]);
+ $app->routeMiddleware([
+     'auth' => App\Http\Middleware\Authenticate::class,
+ ]);
 
 /*
 |--------------------------------------------------------------------------
@@ -85,8 +85,11 @@ $app->singleton(
 |
 */
 
-// $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\AuthServiceProvider::class);
+ $app->register(App\Providers\AppServiceProvider::class);
+
+ $app->register(App\Providers\AuthServiceProvider::class);
+ $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
+
 // $app->register(App\Providers\EventServiceProvider::class);
 
 /*
