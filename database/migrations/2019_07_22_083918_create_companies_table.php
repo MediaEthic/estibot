ec44d157214948fc4ca22cbda4270dadeb37c307
MediaEthic <emilie.rozis@erp-ethic.com>
2019-10-15 12:12:05 +0200

    :card_file_box: Adding companies related changes

---
+++ database/migrations/2019_07_22_083918_create_companies_table.php
@@ -0,0 +1,72 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name', 38);
+            $table->string('logo', 200)->nullable()->default(null);
+            $table->string('phone', 20)->nullable()->default(null);
+            $table->string('email')->nullable()->default(null);
+            $table->string('address_line1', 38)->nullable()->default(null);
+            $table->string('address_line2', 38)->nullable()->default(null);
+            $table->string('address_line3', 38)->nullable()->default(null);
+            $table->string('zipcode', 38)->nullable()->default(null);
+            $table->string('city', 38)->nullable()->default(null);
+            $table->unsignedInteger('country_id')->nullable()->default('75');
+//            $table->foreign('country_id')
+//                ->references('id')
+//                ->on('countries')
+//                ->onDelete('restrict')
+//                ->onUpdate('restrict');
+            $table->unsignedInteger('language_id')->nullable()->default('47');
+//            $table->foreign('language_id')
+//                ->references('id')
+//                ->on('languages')
+//                ->onDelete('restrict')
+//                ->onUpdate('restrict');
+            $table->unsignedInteger('time_zone_id')->nullable()->default('40');
+//            $table->foreign('time_zone_id')
+//                ->references('id')
+//                ->on('time_zones')
+//                ->onDelete('restrict')
+//                ->onUpdate('restrict');
+            $table->string('legal_form', 45)->nullable()->default(null);
+            $table->string('siret', 45)->nullable()->default(null);
+            $table->string('tva', 45)->nullable()->default(null);
+            $table->text('head_quotation')->nullable()->default(null);
+            $table->text('foot_quotation')->nullable()->default(null);
+            $table->text('gsc')->nullable()->default(null);
+            $table->text('subject_email')->nullable()->default(null);
+            $table->text('body_email')->nullable()->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+//        Schema::table('companies', function (Blueprint $table) {
+//            $table->dropForeign(['country_id']);
+//            $table->dropForeign(['language_id']);
+//            $table->dropForeign(['time_zone_id']);
+//        });
+
+        Schema::dropIfExists('companies');
+    }
+}
---
+++ database/seeds/CompaniesTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CompaniesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('companies')->insert([
+            'name' => 'Ethic SAS',
+            'logo' => 'logo-ethic-software.png',
+            'phone' => '+33 (0)5 59 20 81 31',
+            'email' => 'info@ethicsoftware.com',
+            'address_line3' => '2 place du Réduit',
+            'zipcode' => '64100',
+            'city' => 'Bayonne',
+            'legal_form' => 'Société par actions simplifiée',
+            'siret' => '429 542 764 00031',
+            'tva' => 'FR85 429542764',
+            'head_quotation' => 'Nous vous remercions de votre demande de prix et vous prions de bien vouloir trouver ci-après nos meilleures conditions pour la réalisation de :',
+            'foot_quotation' => 'Nous vous remercions de votre demande de prix et vous prions de bien vouloir trouver ci-après nos meilleures conditions pour la réalisation de :',
+            'gsc' => '',
+        ]);
+    }
+}
