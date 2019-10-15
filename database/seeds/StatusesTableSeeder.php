f22972c50f0846af3a5427c274955075df1954f7
MediaEthic <emilie.rozis@erp-ethic.com>
2019-10-15 12:17:53 +0200

    :card_file_box: Adding statuses related changes

---
+++ database/seeds/StatusesTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class StatusesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('statuses')->insert([
+            'name' => 'Devis créé',
+        ]);
+
+        DB::table('statuses')->insert([
+            'name' => 'Devis transmis',
+        ]);
+
+        DB::table('statuses')->insert([
+            'name' => 'Devis relancé',
+        ]);
+
+        DB::table('statuses')->insert([
+            'name' => 'Devis archivé',
+        ]);
+    }
+}
