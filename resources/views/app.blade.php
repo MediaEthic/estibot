29676e9426fdf6ac3f5eda22bb80445a3a4a7b80
MediaEthic <emilie.rozis@erp-ethic.com>
2020-04-22 12:01:13 +0200

    :bento: Adding favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,25 @@
         <title>Estibot - Application web de devis simple et rapide</title>
         <link rel="stylesheet" href="dist/app.css" />
 {{--        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDYypfwfQ1jmY90EMBdo4WUgXfO7rFd9eE&libraries=places"></script>--}}
+
+    <!-- Favicon -->
+        <link rel="apple-touch-icon" sizes="57x57" href="{{ url('assets/img/favicon/apple-icon-57x57.png') }}">
+        <link rel="apple-touch-icon" sizes="60x60" href="{{ url('assets/img/favicon/apple-icon-60x60.png') }}">
+        <link rel="apple-touch-icon" sizes="72x72" href="{{ url('assets/img/favicon/apple-icon-72x72.png') }}">
+        <link rel="apple-touch-icon" sizes="76x76" href="{{ url('assets/img/favicon/apple-icon-76x76.png') }}">
+        <link rel="apple-touch-icon" sizes="114x114" href="{{ url('assets/img/favicon/apple-icon-114x114.png') }}">
+        <link rel="apple-touch-icon" sizes="120x120" href="{{ url('assets/img/favicon/apple-icon-120x120.png') }}">
+        <link rel="apple-touch-icon" sizes="144x144" href="{{ url('assets/img/favicon/apple-icon-144x144.png') }}">
+        <link rel="apple-touch-icon" sizes="152x152" href="{{ url('assets/img/favicon/apple-icon-152x152.png') }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ url('assets/img/favicon/apple-icon-180x180.png') }}">
+        <link rel="icon" type="image/png" sizes="192x192"  href="{{ url('assets/img/favicon/android-icon-192x192.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ url('assets/img/favicon/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="96x96" href="{{ url('assets/img/favicon/favicon-96x96.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ url('assets/img/favicon/favicon-16x16.png') }}">
+        <link rel="manifest" href="{{ url('assets/img/favicon/manifest.json') }}">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="{{ url('assets/img/favicon/ms-icon-144x144.png') }}">
+        <meta name="theme-color" content="#ffffff">
     </head>
 
     <body>
